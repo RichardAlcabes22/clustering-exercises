3e5e1327fdfd8291849655109d4be57246bfb0ac
Rich Alcabes <alcabes.richard@gmail.com>
2023-04-12 16:56:45 -0500

    added clustering_explore

--- wrangle_zillow.py
+++ wrangle_zillow.py
@@ -92,9 +92,10 @@ def handle_missing_values(df, prop_required_column, prop_required_row):
     drop the missing values per the axis contingent on the amount of data present.
     '''
     n_required_column = round(df.shape[0] * prop_required_column)
+    df = df.dropna(axis=1, thresh=n_required_column)
     n_required_row = round(df.shape[1] * prop_required_row)
     df = df.dropna(axis=0, thresh=n_required_row)
-    df = df.dropna(axis=1, thresh=n_required_column)
+    
     return df
 
 def acquire():
