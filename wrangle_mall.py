e1fe1ff9fc4eefbb817529dd179729cc32a0c6b5
Rich Alcabes <alcabes.richard@gmail.com>
2023-04-12 12:02:45 -0500

    edits to data_wrangle_ex

--- wrangle_mall.py
+++ wrangle_mall.py
@@ -1,238 +1,132 @@
+### IMPORTS ####
 import pandas as pd
-import numpy as np
-import os
-from env import user, pwd, host
-import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+
+import env
+
+################
+
+### FUNCTIONS ###
+
+def acquire():
+    database = 'mall_customers'
+    url = f'mysql+pymysql://{env.user}:{env.password}@{env.host}/{database}'
+    df = pd.read_sql('SELECT * FROM customers', url, index_col='customer_id')
+    return df
+
+def one_hot_encode(df):
+    df['is_female'] = df.gender == 'Female'
+    df = df.drop(columns='gender')
+    return df
+
+def split(df):
+    train_and_validate, test = train_test_split(df, random_state=13, test_size=.15)
+    train, validate = train_test_split(train_and_validate, random_state=13, test_size=.2)
+
+    print('Train: %d rows, %d cols' % train.shape)
+    print('Validate: %d rows, %d cols' % validate.shape)
+    print('Test: %d rows, %d cols' % test.shape)
+
+    return train, validate, test
+
+def scale(train, validate, test):
+    columns_to_scale = ['age', 'spending_score', 'annual_income']
+    train_scaled = train.copy()
+    validate_scaled = validate.copy()
+    test_scaled = test.copy()
+
+    scaler = MinMaxScaler()
+    scaler.fit(train[columns_to_scale])
+
+    train_scaled[columns_to_scale] = scaler.transform(train[columns_to_scale])
+    validate_scaled[columns_to_scale] = scaler.transform(validate[columns_to_scale])
+    test_scaled[columns_to_scale] = scaler.transform(test[columns_to_scale])
+
+    return scaler, train_scaled, validate_scaled, test_scaled
+
+def overview(df):
+    print('--- Shape: {}'.format(df.shape))
+    print()
+    print('--- Info')
+    df.info()
+    print()
+    print('--- Column Descriptions')
+    print(df.describe(include='all'))
+
+def handle_outliers(df, cols, k):
+    # Create placeholder dictionary for each columns bounds
+    bounds_dict = {}
+
+    # get a list of all columns that are not object type
+    non_object_cols = df.dtypes[df.dtypes != 'object'].index
 
 
-def acquire_df():
-    ''' 
-     Acquire data from X schema using env imports, rename columns, and storing a cached version of SQL pull as a .csv.
-     Specifically, the SQL query returns a df in accordance with SQL script.
-     ''' 
-    if os.path.exists('cust.csv'):
-        print('local version found!')
-        return pd.read_csv('cust.csv', index_col=0)
+    for col in non_object_cols:
+        # get necessary iqr values
+        q1 = df[col].quantile(0.25)
+        q3 = df[col].quantile(0.75)
+        iqr = q3 - q1
+        upper_bound =  q3 + k * iqr
+        lower_bound =  q1 - k * iqr
+
+        #store values in a dictionary referencable by the column name
+        #and specific bound
+        bounds_dict[col] = {}
+        bounds_dict[col]['upper_bound'] = upper_bound
+        bounds_dict[col]['lower_bound'] = lower_bound
+
+    for col in non_object_cols:
+        #retrieve bounds
+        col_upper_bound = bounds_dict[col]['upper_bound']
+        col_lower_bound = bounds_dict[col]['lower_bound']
+
+        #remove rows with an outlier in that column
+        df = df[(df[col] < col_upper_bound) & (df[col] > col_lower_bound)]
+    
+    return df
+
+def handle_missing_values(df, prop_required_column, prop_required_row):
+    n_required_column = round(df.shape[0] * prop_required_column)
+    n_required_row = round(df.shape[1] * prop_required_row)
+    df = df.dropna(axis=0, thresh=n_required_row)
+    df = df.dropna(axis=1, thresh=n_required_column)
+    return df
+
+def get_exploration_data():
+    df = acquire()
+    
+    print('Before dropping nulls, %d rows, %d cols' % df.shape)
+    df = handle_missing_values(df, prop_required_column=.5, prop_required_row=.5)
+    print('After dropping nulls, %d rows, %d cols' % df.shape)
+    
+    train, validate, test = split(df)
+    
+    return train
+
+def get_modeling_data(scale_data=False):
+    df = acquire()
+    
+    print('Before dropping nulls, %d rows, %d cols' % df.shape)
+    df = handle_missing_values(df, prop_required_column=.5, prop_required_row=.5)
+    print('After dropping nulls, %d rows, %d cols' % df.shape)
+    
+    print()
+    
+    print('Before removing outliers, %d rows, %d cols' % df.shape)
+    df = handle_outliers(df, ['age', 'spending_score', 'annual_income'], 1.5)
+    print('After dropping outliers, %d rows, %d cols' % df.shape)
+    
+    print()
+    
+    df = one_hot_encode(df)
+    
+    train, validate, test = split(df)
+    
+    if scale_data:
+        return scale(train, validate, test)
     else:
-        url = f"mysql+pymysql://{user}:{pwd}@{host}/mall_customers"
-
-        query = """
-
-        SELECT *
-        FROM customers;  
-
-        """
-        df = pd.read_sql(query, url)
-        df.to_csv('cust.csv',index=True)
-        return df
-
-def nulls_by_col(df):
-    '''
-    This function takes in a dataframe 
-    and finds the number of missing values in each column
-    it returns a dataframe with quantity and percent of missing values
-    '''
-    num_missing = df.isnull().sum()
-    percent_miss = num_missing / df.shape[0] * 100
-    cols_missing = pd.DataFrame({'num_rows_missing': num_missing, 'percent_rows_missing': percent_miss})
-    return cols_missing.sort_values(by='num_rows_missing', ascending=False)
-
-def nulls_by_row(df):
-    '''
-    This function takes in a dataframe 
-    and finds the number of missing values in a row
-    it returns a new dataframe with quantity and percent of missing values
-    '''
-    num_missing = df.isnull().sum(axis=1)
-    percent_miss = num_missing / df.shape[1] * 100
-    rows_missing = pd.DataFrame({'num_cols_missing': num_missing, 'percent_cols_missing': percent_miss})
-    rows_missing = df.merge(rows_missing,
-                        left_index=True,
-                        right_index=True)[['num_cols_missing', 'percent_cols_missing']]
-    return rows_missing.sort_values(by='num_cols_missing', ascending=False)
-
-
-
-def summarize(df):
-    '''
-    summarize will take in a single argument (a pandas dataframe) 
-    and output to console various statistics on said dataframe, including:
-    # .head()
-    # .info()
-    # .describe()
-    # .value_counts()
-    # observation of nulls in the dataframe
-    '''
-    print('SUMMARY REPORT')
-    print('=====================================================\n\n')
-    print('Dataframe head: ')
-    print(df.head(5))
-    print('=====================================================\n\n')
-    print('Dataframe info: ')
-    print(df.info())
-    print('=====================================================\n\n')
-    print('Dataframe Description: ')
-    print(df.describe())
-    num_cols = [col for col in df.columns if df[col].dtype != 'O']
-    cat_cols = [col for col in df.columns if col not in num_cols]
-    print('=====================================================')
-    print('DataFrame value counts: ')
-    for col in df.columns:
-        if col in cat_cols:
-            print(df[col].value_counts(), '\n')
-        else:
-            print(df[col].value_counts(bins=10, sort=False), '\n')
-    print('=====================================================')
-    print('nulls in dataframe by column: ')
-    print(nulls_by_col(df))
-    print('=====================================================')
-    print('nulls in dataframe by row: ')
-    print(nulls_by_row(df))
-    print('=====================================================')
-
-def visualize_outliers(df):
-    ax = sns.boxplot(data=df, orient='h',palette='Set2')
-    return ax
-
-def get_upper_outliers(s, k=1.5):
-    '''
-    Given a series (ie df.col_name) and a cutoff value, k, returns the upper outliers for the
-    series.
-
-    The values returned will be either 0 (if the point is not an outlier), or a
-    number that indicates how far away from the upper bound the observation is.
-    '''
-    q1, q3 = s.quantile([.25, 0.75])
-    iqr = q3 - q1
-    upper_bound = q3 + k * iqr
-    return s.apply(lambda x: max([x - upper_bound, 0]))
-
-
-def add_upper_outlier_columns(df, k=1.5):
-    '''
-    Add a column with the suffix _outliers for all the numeric columns
-    in the given dataframe.
-    '''
-    for col in df.select_dtypes('number'):
-        df[col + '_outliers_upper'] = get_upper_outliers(df[col], k)
-    return df
-
-def remove_outliers(df, k, col_list):
-    ''' Remove outliers from a list of columns in a dataframe 
-        and return that dataframe.  This function takes in a dataframe, a multiplier applied to IQR used to define
-        an outlier (recommend 1.5), and finally a list of columns subjected to the function.
-        
-        Outputs a dataframe with any rows containing outliers in any of the specified columns REMOVED from the
-        input dataframe.
-    '''
+        return train, validate, test
     
-    for col in col_list:
-
-        q1, q3 = df[col].quantile([.25, .75])  # get quartiles
-        
-        iqr = q3 - q1   # calculate interquartile range
-        
-        upper_bound = q3 + k * iqr   # get upper bound
-        lower_bound = q1 - k * iqr   # get lower bound
-
-        # return dataframe without outliers
-        
-        df = df[(df[col] > lower_bound) & (df[col] < upper_bound)]
-        
-    return df
-
-
-def train_validate_test(df):
-    # split df into test (20%) and train_validate (80%)
-    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
-
-# split train_validate off into train (70% of 80% = 56%) and validate (30% of 80% = 24%)
-    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
-    return train, validate,test
-
-def get_object_cols(df):
-    '''
-    This function takes in a dataframe and identifies the columns that are object types
-    and returns a list of those column names. 
-    '''
-    # create a mask of columns whether they are object type or not
-    mask = np.array(df.dtypes == "object")
-
-        
-    # get a list of the column names that are objects (from the mask)
-    object_cols = df.iloc[:, mask].columns.tolist()
-    
-    return object_cols
-
-
-
-def create_dummies(df, object_cols):
-    '''
-    This function takes in a dataframe and list of object column names,
-    and creates dummy variables of each of those columns. 
-    It then appends the dummy variables to the original dataframe. 
-    It returns the original df with the appended dummy variables. 
-    '''
-    
-    # run pd.get_dummies() to create dummy vars for the object columns. 
-    # we will drop the column representing the first unique value of each variable
-    # we will opt to not create na columns for each variable with missing values 
-    # (all missing values have been removed.)
-    dummy_df = pd.get_dummies(object_cols, dummy_na=False, drop_first=True)
-    
-    # concatenate the dataframe with dummies to our original dataframe
-    # via column (axis=1)
-    df = pd.concat([df, dummy_df], axis=1)
-
-    return df
-
-def handle_missing_values(df, prop_required_columns=0.5, prop_required_rows=0.75):
-    '''
-    This function takes in a dataframe, the percent of columns and rows
-    that need to have values/non-nulls
-    and returns the dataframe with the desired amount of nulls left.
-    '''
-    column_threshold = int(round(prop_required_columns * len(df.index), 0))
-    df = df.dropna(axis=1, thresh=column_threshold)
-    row_threshold = int(round(prop_required_rows * len(df.columns), 0))
-    df = df.dropna(axis=0, thresh=row_threshold)
-    return df
-
-def min_max_scale(X_train, X_validate, X_test, numeric_cols):
-    '''
-    this function takes in 3 dataframes with the same columns, 
-    a list of numeric column names (because the scaler can only work with numeric columns),
-    and fits a min-max scaler to the first dataframe and transforms all
-    3 dataframes using that scaler. 
-    it returns 3 dataframes with the same column names and scaled values. 
-    '''
-    # create the scaler object and fit it to X_train (i.e. identify min and max)
-    # if copy = false, inplace row normalization happens and avoids a copy (if the input is already a numpy array).
-
-
-    scaler = MinMaxScaler(copy=True).fit(X_train[numeric_cols])
-
-    #scale X_train, X_validate, X_test using the mins and maxes stored in the scaler derived from X_train. 
-    # 
-    X_train_scaled_array = scaler.transform(X_train[numeric_cols])
-    X_validate_scaled_array = scaler.transform(X_validate[numeric_cols])
-    X_test_scaled_array = scaler.transform(X_test[numeric_cols])
-
-    # convert arrays to dataframes
-    X_train_scaled = pd.DataFrame(X_train_scaled_array, 
-                                  columns=numeric_cols).\
-                                  set_index([X_train.index.values])
-
-    X_validate_scaled = pd.DataFrame(X_validate_scaled_array, 
-                                     columns=numeric_cols).\
-                                     set_index([X_validate.index.values])
-
-    X_test_scaled = pd.DataFrame(X_test_scaled_array, 
-                                 columns=numeric_cols).\
-                                 set_index([X_test.index.values])
-
-    
-    return X_train_scaled, X_validate_scaled, X_test_scaled
+#################
